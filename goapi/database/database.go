632086c9ddc6abeb4896c54cdec39554362028b7
SAGNIK PAL <palsagnik2102@gmail.com>
2024-06-25 01:17:59 +0530

    Feature: DB config files and connection

--- goapi/database/database.go
+++ goapi/database/database.go
@@ -3,18 +3,20 @@ package database
 import (
 	"database/sql"
 	"fmt"
+
 	_ "github.com/lib/pq"
+	"github.com/palSagnik/zephyr/config"
 )
 
 
 func ConnectDB() error {
 
-	connectionString := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, database)
+	connectionString := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", config.DB_HOST, config.DB_PORT, config.DB_USER, config.DB_PASS, config.DB_NAME)
 
 	db, err := sql.Open("postgres", connectionString)
 	if err != nil {
 		return err
 	}
 
-
+	return nil
 }
--- goapi/go.mod
+++ goapi/go.mod
@@ -3,6 +3,7 @@ module github.com/palSagnik/zephyr
 go 1.22.3
 
 require (
+	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
 	k8s.io/client-go v0.30.2
 )
