6a12501e3a7d81b2e243e88fd26b17993dffc3a7
SAGNIK PAL <palsagnik2102@gmail.com>
2024-07-10 22:13:22 +0530

    UPDATE: instance config setup

--- goapi/config/instance.go
+++ goapi/config/instance.go
@@ -1,6 +1,9 @@
 package config
 
-import "os"
+import (
+	"os"
+	"strconv"
+)
 
 var CPU_REQUEST = os.Getenv("CPU_REQUEST")
 var MEMORY_REQUEST = os.Getenv("MEMORY_REQUEST")
@@ -9,3 +12,10 @@ var MEMORY_LIMIT = os.Getenv("MEMORY_LIMIT")
 var DISK_REQUEST = os.Getenv("DISK_REQUEST")
 var DISK_LIMIT = os.Getenv("DISK_LIMIT")
 var TERMINATION_PERIOD = os.Getenv("TERMINATION_PERIOD")
+
+var CONCURRENT_INSTANCES, _ = strconv.Atoi(os.Getenv("CONCURRENT_INSTANCES"))
+var INSTANCE_NAME_SECRET = os.Getenv("INSTANCE_NAME_SECRET")
+var DEFAULT_USERNAME = os.Getenv("DEFAULT_USERNAME")
+var INSTANCE_HOSTNAME = os.Getenv("INSTANCE_HOSTNAME")
+var INSTANCE_TIME, _ = strconv.Atoi(os.Getenv("INSTANCE_TIME"))
+var MAX_INSTANCE_TIME, _ = strconv.Atoi(os.Getenv("MAX_INSTANCE_TIME"))
